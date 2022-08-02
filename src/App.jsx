5b44cea94b26b54f0e309bc4a4fbb1de7c9959da
Wildes Sousa <74382948+Sousa-Diin@users.noreply.github.com>
2022-08-02 01:54:25 +0000

    :memo: :rocket: Primeiro commit

--- src/App.jsx
+++ src/App.jsx
@@ -15,18 +15,28 @@ function App() {
 				<div className="container-cria-login">
 					<h2>Vamos Criar Seu Acesso!</h2>
 					<form className="form-cria-login"  method="post" action="">
-						<input className="email" id="email_cadastro" name="email_ca" type="email"  placeholder="*Email: contato@gmail.com"></input>
-						<input className="senha" id="senha" name="senha" type="password" placeholder="*Senha:"></input>
-						<input className="senha"id="confirmar_senha" name="confirmar_senha" type="password" placeholder="*Confirmar senha:"></input>
+						<input className="campo-email" id="email_cadastro" name="email_ca" type="email"  placeholder="*Email: contato@gmail.com"></input>
+						<input className="campo-senha" id="senha" name="senha" type="password" placeholder="*Senha:"></input>
+						<input className="campo-senha"id="confirmar_senha" name="confirmar_senha" type="password" placeholder="*Confirmar senha:"></input>
 					</form>
 				</div>
 
 				<div className="container-dados-pessoais">
 					<h2>Seus Dados Pessoais:</h2>
-					<form className="form-dados-pessoais"/>
+					<form className="form-dados-pessoais">
+						<input className="campo-name"  name="nome" type="email"  placeholder="*Nome completo:"></input>
+						
+						<input className="campo-nasc" name="data-nasc" type="date" placeholder="*Data de Nascimento:"></input>
+						<input className="campo-sex"  name="sex" type="Text" placeholder="*Sexo:"></input>
+						<input className="campo-tel"  name="tel" type="tel" placeholder="*Telefone:"></input>
+					</form>
 				</div>
-				<input type="image" name="tEnviar" src="_imagens/botao-enviar.png"/>
+				
 			</div>
+			
+		</div>
+		<div id="btn-proximo">
+			<input className="botao" type="submit" name="tProximo" ></input>
 		</div>
     </main>
   );
